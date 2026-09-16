86b176e60b86e42d9c1f0f97d4964a757a1a2909
arsham-sh <arsham.shabani.dev@gmail.com>
2026-09-16 23:25:40 +0330

    Add LiveKit Laravel configuration

---
+++ config/livekit.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'url' => env('LIVEKIT_URL', 'http://localhost:7880'),
+    'api_key' => env('LIVEKIT_API_KEY', 'devkey'),
+    'api_secret' => env('LIVEKIT_API_SECRET', 'secret'),
+];
