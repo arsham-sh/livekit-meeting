ff729a2fbe1cda5e3c2b9a6d7b3716410b390538
arsham-sh <arsham.shabani.dev@gmail.com>
2026-09-16 23:25:44 +0330

    Add meeting controller and token endpoint

---
+++ app/Http/Controllers/MeetingController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\LiveKitService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Support\Str;
+
+class MeetingController extends Controller
+{
+    public function create(): View
+    {
+        return view('meetings.create');
+    }
+
+    public function store(): mixed
+    {
+        $room = Str::lower(Str::random(12));
+
+        return redirect()->route('meetings.show', ['room' => $room]);
+    }
+
+    public function show(string $room): View
+    {
+        return view('meetings.show', [
+            'room' => $room,
+            'livekitUrl' => config('livekit.url'),
+        ]);
+    }
+
+    public function token(Request $request, string $room, LiveKitService $liveKit): JsonResponse
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'min:1', 'max:80'],
+        ]);
+
+        $identity = (string) Str::uuid();
+
+        return response()->json([
+            'server_url' => config('livekit.url'),
+            'participant_token' => $liveKit->token($room, $identity, $data['name']),
+        ]);
+    }
+}
