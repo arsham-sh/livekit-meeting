9885b3282b3c23107b126514cf169ed8f861c93f
arsham-sh <arsham.shabani.dev@gmail.com>
2026-09-20 01:39:09 +0330

    Rate limit meeting token issuance

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MeetingController::class, 'create'])->name('meetings.create');
 Route::post('/meetings', [MeetingController::class, 'store'])->name('meetings.store');
 Route::get('/meetings/{room}', [MeetingController::class, 'show'])->where('room', '[A-Za-z0-9_-]+')->name('meetings.show');
-Route::post('/meetings/{room}/token', [MeetingController::class, 'token'])->where('room', '[A-Za-z0-9_-]+')->name('meetings.token');
+Route::post('/meetings/{room}/token', [MeetingController::class, 'token'])
+    ->middleware('throttle:10,1')
+    ->where('room', '[A-Za-z0-9_-]+')
+    ->name('meetings.token');
